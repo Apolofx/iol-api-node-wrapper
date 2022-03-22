032a3ac862dff9640de85fbf69bab74e579f2485
Nacho <nacho.conso@gmail.com>
2022-03-22 00:04:01 -0300

    add panel and cotizacion types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,5 @@ export * from "./IolApiCuenta";
 export * from "./IolApiOperar";
 export * from "./IolApiTitulos";
 export * from "./IolClient";
+export * from "./IolApiCotizaciones";
+export * from "./IolApiPaneles";
